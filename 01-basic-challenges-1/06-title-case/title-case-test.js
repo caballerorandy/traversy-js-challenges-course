7aa7686ee78e00d041a984d92a1cb374cc168db1
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2023-10-30 22:25:00 -0500

    06 done

--- 01-basic-challenges-1/06-title-case/title-case-test.js
+++
@@ -1,7 +0,0 @@
-const titleCase = require('./title-case');
-
-test('Converting string to title case', () => {
-  expect(titleCase('hello world')).toBe('Hello World');
-  expect(titleCase('javascript programming')).toBe('Javascript Programming');
-  expect(titleCase('openai chatbot')).toBe('Openai Chatbot');
-});
