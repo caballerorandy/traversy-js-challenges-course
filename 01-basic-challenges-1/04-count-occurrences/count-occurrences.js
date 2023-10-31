25167e1a40c1618a6b2ca21f95c6878f242af498
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2023-10-30 21:51:11 -0500

    04 done

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
+//const countOccurrences = (string, char) => string.split(char).length - 1;
 
 module.exports = countOccurrences;
