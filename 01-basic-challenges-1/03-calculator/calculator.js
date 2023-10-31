379c0e3261164fb4fbfdf3b5649a656c0c90178d
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2023-10-30 21:37:37 -0500

    03 done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,37 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      throw new Error("Invalid operator");
+      break;
+  }
+  return result;
+}
+
+//   if (operator === "+") {
+//     result = num1 + num2;
+//   } else if (operator === "-") {
+//     result = num1 - num2;
+//   } else if (operator === "*") {
+//     result = num1 * num2;
+//   } else if (operator === "/") {
+//     result = num1 / num2;
+//   } else {
+//     throw new Error("Invalid operator");
+//   }
+//   return result;
+// }
 
 module.exports = calculator;
