b54cc9e10b9281f9ae4a656b3137f79038c29acc
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2023-10-30 22:04:52 -0500

    05 done

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,15 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  // let maxNum = arr[0];
+  // for (let i = 1; i < arr.length; i++) {
+  //   if (maxNum < arr[i]) {
+  //     maxNum = arr[i];
+  //   }
+  // }
+  // return maxNum;
+
+  return Math.max(...arr);
+}
+
+console.log(findMaxNumber([1, 4, 8, 2, 4, 94, 1, 4, 5, 3]));
 
 module.exports = findMaxNumber;
--- 01-basic-challenges-1/05-find-max-number/find-max-number.test.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.test.js
@@ -1,6 +1,6 @@
-const findMaxNumber = require('./find-max-number');
+const findMaxNumber = require("./find-max-number");
 
-test('Finding the maximum number in an array', () => {
+test("Finding the maximum number in an array", () => {
   expect(findMaxNumber([1, 5, 3, 9, 2])).toBe(9);
   expect(findMaxNumber([0, -1, -5, 2])).toBe(2);
   expect(findMaxNumber([10, 10, 10, 10])).toBe(10);
